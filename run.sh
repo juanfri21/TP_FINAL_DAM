b24bbbfac233c3c1556188bababe484ba21ba317
juan <juanfri_tentor@hotmail.com>
2020-08-30 11:54:54 -0300

    modificado run.sh

--- run.sh
+++ run.sh
@@ -3,18 +3,19 @@
 echo "Cerrando posibles contenedores abiertos"
 docker stop $(docker ps -a -q)
 echo "Eliminando red"
-docker network rm mysql-net
+docker network rm mysql-net &
+sleep 5
 echo "Creando red"
 docker network create --driver bridge mysql-net &
 sleep 5
 cd apiNodejs
-echo "Levantando contenedor phpadmin en puerto 8085"
-./run_phpadmin.sh mysql-net mysql-server 8085 &
-sleep 5
-echo "Levantando contenedor mysql"
+echo "Iniciando base de datos mysql"
 ./start_mysql.sh mysql-net "$PWD"/db &
 sleep 5
-echo "Levantando contenedor nodejs en puerto 3000"
+echo "Iniciando phpadmin en puerto 8085"
+./run_phpadmin.sh mysql-net mysql-server 8085 &
+sleep 5
+echo "Iniciando nodejs en puerto 3000"
 ./serve_node_app_net.sh "$PWD" index.js 3000 mysql-net &
 sleep 5
 cd ../ionicProject
